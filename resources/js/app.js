91f517129810843d82162bff756f68d78e5f85bb
Andrew kodeksoff@gmail.com <kodeksoff@gmail.com>
2021-07-08 13:51:36 +0300

    add vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue'
+import store from './store'
+
+
+
+Vue.component('login', require('./components/header/login').default)
+Vue.component('mobile-menu', require('./components/header/mobile-menu').default)
+Vue.component('mobile-menu-button', require('./components/header/mobile-menu-button').default)
 
 const app = new Vue({
-    el: '#app',
-});
+    store,
+}).$mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,4 @@
-<x-layout>
-    <div
+ <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -155,4 +154,3 @@
             </div>
         </div>
     </div>
-</x-layout>
